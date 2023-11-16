79d0539c617cdd8d5307c9fb9404a5427c2104a9
cailong <cailong.lcl@gmail.com>
2023-11-16 11:21:31 +0800

    feat: 更新vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import vitePluginImp from 'vite-plugin-imp';
+// import vitePluginImp from 'vite-plugin-imp';
 
 import proxyConfig from './config/proxy';
 import projectConfig from './config/config';
@@ -19,14 +19,15 @@ export default defineConfig(({ mode }) => {
           },
         },
       }),
-      vitePluginImp({
-        libList: [
-          {
-            libName: 'antd',
-            style: (name) => `antd/es/${name}/style`,
-          },
-        ],
-      }),
+
+      // vitePluginImp({
+      //   libList: [
+      //     {
+      //       libName: 'antd',
+      //       style: (name) => `antd/es/${name}/style`,
+      //     },
+      //   ],
+      // }),
     ],
     resolve: {
       alias: {
